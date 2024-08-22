702052de48aadef6cd3dee6235685c99afda6eec
Ethan Chen <Ethanchen4@gmail.com>
2024-08-22 12:47:16 -0400

    Added thread model

---
+++ threadModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const threadSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now() },
+});
+
+const threadModel = mongoose.model("Thread", threadSchema);
+module.exports = threadModel;
